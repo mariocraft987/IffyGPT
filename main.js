1bd48f602ce632cbed80bcf026a602c312960677
mariocraft <154646419+mariocraft987@users.noreply.github.com>
2024-04-29 09:49:19 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
-let known = ["apple", "orange", "banana", "pear", "blueberry", "pineapple"]
-known += ["dog", "cat", "snake", "parrot", "bird", "hamster", "horse", "pig"]
-known += ["creators"]
+/*
+* IffyGPT
+* version 0.2
+*/
+
+let known = ["apple", "orange", "banana", "pear", "blueberry", "pineapple"];
+known += ["dog", "cat", "snake", "parrot", "bird", "hamster", "horse", "pig"];
 
 function askIffy(text) {
-  if(text.includes('creators')) { return "Oh, my creator is mariocraft987!" }
+  let q = text.toLowerCase();
+  if(q.trim() == "") { return "You're asking me nothing!?"; }
+  if(q.includes('creator')) { return "Oh, my creator is mariocraft987!"; }
 }
 
 export known;
