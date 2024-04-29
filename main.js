4ce2b1b4a808319d99856f6fb663e4737b92f906
mariocraft <154646419+mariocraft987@users.noreply.github.com>
2024-04-29 09:37:48 -0700

    Update main.js

--- main.js
+++ main.js
@@ -2,4 +2,8 @@ let known = ["apple", "orange", "banana", "pear", "blueberry", "pineapple"]
 known += ["dog", "cat", "snake", "parrot", "bird", "hamster", "horse", "pig"]
 known += ["creators"]
 
+function askIffy(text) {
+  if(text.includes('creators')) { return "Oh, my creator is mariocraft987!" }
+}
+
 export known;
